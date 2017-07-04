a691d59c4cd6240aaaaa51342d2a0100bcf9caa8
kalep <kalep@KALEP->
2017-07-05 00:38:59 +0300

    Добавьте файлы проекта.

---
+++ Homework2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\n Введите сколько у вас денег =  ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n1 бутылка воды стоит 2$\n1 булочка стоит = 1$ ");
+            Console.Write("\n Сколько бутылок воды желаете приобрести? = ");
+            int waterCol = int.Parse(Console.ReadLine());
+
+            Console.Write("\n Сколько булочек хотите скушать? = ");
+            int cookiesCol = int.Parse(Console.ReadLine());
+
+            // цена
+            int water = 2 * waterCol;
+            int cookies = 1 * cookiesCol;
+
+            if ((n - (water + cookies)) >= 0)
+                Console.Write($"Сумма к оплате ={(water + cookies)} Ваш остаток = { n - (water + cookies) }");
+            else
+                Console.Write($"Сумма к оплате ={(water + cookies)}, Извините, но вам не хватает {n - (water + cookies)}$ ");
+            Console.ReadKey();
+        }
+
+    }
+
+}
